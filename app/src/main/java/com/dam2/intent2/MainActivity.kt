9f31a9a41cdd019f7ee18f7f21847987a22df803
ETLuis <lcampanogoldaro@gmail.com>
2023-01-23 12:59:26 +0100

    Añadidos comentarios y añadida funcion de los numeros.

--- app/src/main/java/com/dam2/intent2/MainActivity.kt
+++ app/src/main/java/com/dam2/intent2/MainActivity.kt
@@ -8,24 +8,46 @@ import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    // Declaramos los resultados del request, el TextView de la imagen y el TextView del resultado
     val RESULTADO_UNO = 1
     val RESULTADO_DOS = 2
-    val imagen = findViewById<ImageView>(R.id.imageView)
-    val resultText = findViewById<TextView>(R.id.resultText)
+    val imagen = findViewById<ImageView>(R.id.imagen)
+    val resultText = findViewById<TextView>(R.id.resultado)
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
-
+            // Declaramos el boton para abrir la camara
             val camara = findViewById<Button>(R.id.camara)
-
+            //Hacemos el evento de la camara
             camara.setOnClickListener() {
+                //Le decimos que haga el metodo "dispatchTakePictureIntent()"
                 dispatchTakePictureIntent()
             }
-    }
+            // Declaramos el boton del resultado
+            val resultado = findViewById<Button>(R.id.resultado)
 
+            //Hacemos el evento del resultado
+            resultado.setOnClickListener(){
+                // Declaramos el Intent y la clase Random
+                val intent = Intent(this, MainActivity2::class.java)
+                val random = Random()
+
+                // Declaramos los numeros que queremos operar
+                val numero1 = random.nextInt(10) + 1
+                val numero2 = random.nextInt(10) + 1
+
+                // Añadimos números al Intent
+                intent.putExtra("numero1", numero1)
+                intent.putExtra("numero2", numero2)
+
+                // Iniciamos la Segunda Activity
+                startActivityForResult(intent, RESULTADO_DOS)
+            }
+    }
+    // Declaramos el boton del resultado
     val REQUEST_IMAGE_CAPTURE = 1
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
@@ -35,14 +57,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    // Declaramos el onActivityResult
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+        // Le decimos que si el requestCode es igual a RESULTADO_UNO nos ponga la imagen en el TextView
         if (requestCode == RESULTADO_UNO && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             imagen.setImageBitmap(imageBitmap)
         }
+        // Le decimos que si el requestCode es igual a RESULTADO_DOS nos haga la operacion de multiplicacion en la Segunda Activity
         if (requestCode == RESULTADO_DOS && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             resultText.text = data?.getStringExtra("multiplicacion")
--- app/src/main/java/com/dam2/intent2/MainActivity2.kt
+++ app/src/main/java/com/dam2/intent2/MainActivity2.kt
@@ -9,17 +9,24 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
+        //Declaramos el TextView del resultado
         val resultText = findViewById<TextView>(R.id.resultado)
+        // Recogemos el Intent
         val intent = getIntent()
+        // Recogemos los valores de los numeros
         val numero1 = intent.getIntExtra("numero1", 0)
         val numero2 = intent.getIntExtra("numero2", 0)
+        // Hacemos la operacion
         var resultado = numero1 * numero2
 
         intent.putExtra("multiplicacion", resultado.toString());
+        //Hacemos un setResult para enviarselo al Intent
         setResult(RESULT_OK, intent);
+        //Cambiamos el resultado en el TextView
         resultText.text = resultado.toString()
+        //Mandamos un mensaje por el LogCat que nos muestra el resultado
         Log.d("Resultado",  numero1 + " * " + numero2 + "Resultado = " + resultado)
+        // Cerramos la activity
         finish()
 
     }
