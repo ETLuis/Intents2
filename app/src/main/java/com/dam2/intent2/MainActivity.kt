31dfd8b8ec6156623f7eb9290d7f445e8c2644a8
ETLuis <lcampanogoldaro@gmail.com>
2023-01-23 12:49:43 +0100

    Añadida función guardar imagen en el TextView.

--- app/src/main/java/com/dam2/intent2/MainActivity.kt
+++ app/src/main/java/com/dam2/intent2/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.dam2.intent2
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-
+        val RESULTADO_UNO = 1
+    val imagen = findViewById<ImageView>(R.id.imageView)
+    val resultText = findViewById<TextView>(R.id.resultText)
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
@@ -27,6 +31,16 @@ class MainActivity : AppCompatActivity() {
             takePictureIntent.resolveActivity(packageManager)?.also {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
             }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RESULTADO_UNO && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imagen.setImageBitmap(imageBitmap)
         }
     }
 }
